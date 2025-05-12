ec3a9fc540642a044b3c695d76a7670fd137d00f
MarlenKoch <marlen.koch@sap.com>
2025-05-12 16:28:01 +0100

    add basic answer functionality to backend

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from db import Base, engine
-from routers import chats, paragraphs, projects
+from routers import chats, paragraphs, projects, answers
 from sourceDocument import generateSourceDocument
 from ai import aiChat, get_models
 
@@ -23,8 +23,9 @@ app.add_middleware(
 app.include_router(chats.router, prefix="/chats", tags=["chats"])
 app.include_router(paragraphs.router, prefix="/paragraphs", tags=["paragraphs"])
 app.include_router(projects.router, prefix="/projects", tags=["projects"])
+app.include_router(answers.router, prefix="/answers", tags=["answers"])
 
-app.add_api_route("/aiChat", aiChat, methods=["POST","PUT"], tags=["ai"])
+app.add_api_route("/aiChat", aiChat, methods=["POST", "PUT"], tags=["ai"])
 
 app.add_api_route("/aimodels", get_models, methods=["GET"], tags=["ai"])
 
