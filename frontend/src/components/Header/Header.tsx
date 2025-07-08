aecf3a3f1596ba46330990993f187bd1459fbbfa
i590648 <amelie.hoffmann@sap.com>
2025-07-08 13:29:16 +0100

    search bar weg und info pop up weg wenn man klickt

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,18 +1,15 @@
 import { Link } from "react-router-dom";
-import styles from "./Header.module.css"
-import SearchBar from "./SearchBar";
+import styles from "./Header.module.css";
 
-const Header: React.FC = () =>{
-
-    return(
+const Header: React.FC = () => {
+  return (
     <div className={styles.header}>
-           <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
-            <img className={styles.logo} src="/logo-test.svg" alt="KI-Logo" />
-          </Link>
-          <h2 className={styles.headerText}>Home Component</h2>
-          <SearchBar/>
-      </div>
-    )
-}
+      <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+        <img className={styles.logo} src="/logo-test.svg" alt="KI-Logo" />
+      </Link>
+      <h2 className={styles.headerText}>Schreibassistent</h2>
+    </div>
+  );
+};
 
 export default Header;
